2fa51c6d526846a9b657c6693721bb5d7abea196
Rochelle Palma <palmarochelleg@gmail.com>
2020-07-29 22:22:31 -0700

    added comments and events endpoints

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const usersRouter = require('./users/users-router');
 const authRouter = require('./auth/auth-router');
 const dogsRouter = require('./dogs/dogs-router');
 const eventsRouter = require('./events/events-router');
+const commentsRouter = require('./comments/comments-router');
 
 const app = express()
 
@@ -25,6 +26,7 @@ app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/dogs', dogsRouter);
 app.use('/api/events', eventsRouter);
+app.use('/api/comments', commentsRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
