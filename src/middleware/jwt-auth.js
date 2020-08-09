aa2c8628f8992e7626f7baffb69efb537ad94feb
Rochelle Palma <palmarochelleg@gmail.com>
2020-08-09 03:42:08 -0700

    modify function name to verify jwt

--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -18,17 +18,19 @@ function requireAuth(req, res, next) {
             payload.sub,
         )
             .then(user => {
-                if (!user)
+                if (!user){
                     return res.status(401).json({ error: 'Unauthorized request' });
+                }
 
                 req.user = user;
                 next();
             })
             .catch(err => {
-                console.error(err);
+                console.log(err);
                 next(err);
             })
-    } catch(error) {
+    } 
+    catch(error) {
         res.status(401).json({ error: 'Unauthorized request' });
     }
 }
