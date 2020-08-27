e52ba7659563394306fd44ab52f6a0b3f231b4e6
Rochelle Palma <palmarochelleg@gmail.com>
2020-08-26 19:28:53 -0700

    deployment

--- postgrator-config.js
+++ postgrator-config.js
@@ -5,7 +5,7 @@ module.exports = {
     "driver": "pg",
     "connectionString": (process.env.NODE_ENV === 'test')
         ? process.env.TEST_DATABASE_URL
-        : process.env.PRODUCTION_DATABASE_URL,
+        : process.env.DATABASE_URL,
         "ssl": !!process.env.SSL,
         validateChecksums: false,
     // "ssl": { rejectUnauthorized: false }
