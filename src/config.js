ca04ad8bcc290305fd510fd61b29d4a4f1e90e16
Rochelle Palma <palmarochelleg@gmail.com>
2020-08-26 19:30:49 -0700

    changes for deployment

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     //API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000/api",
-    DATABASE_URL: process.env.PRODUCTION_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/doggodate',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/doggodate',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/doggodate-test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
