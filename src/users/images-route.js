0518a8f082563b21100560ceb25f102f425bf2fe
Rochelle Palma <palmarochelleg@gmail.com>
2020-09-01 21:34:58 -0700

    changes Bucket to bucket

--- src/users/images-route.js
+++ src/users/images-route.js
@@ -10,7 +10,7 @@ const router = express.Router();
 const s3 = new aws.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    Bucket: process.env.BUCKET
+    bucket: process.env.BUCKET
    });
 
    const profileImgUpload = multer({
